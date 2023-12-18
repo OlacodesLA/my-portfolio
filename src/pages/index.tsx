5b33c27b29e4dba88110406b21cc456c8ab01ed6
OlacodesLA <zaccheausjide@gmail.com>
2023-12-18 04:12:25 +0100

    :tada: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+    >
+      <h1>My Portfolio</h1>
+      <p>Olajide Zaccheaus</p>
+    </main>
+  );
+}
